8f818e6b2282158a73a8a2d9bf99bd6ef76f37d5
github username <github email address>
2019-11-04 15:06:12 -0500

    Done.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,23 +1,33 @@
 import React from 'react';
 
-const SearchBar = () => {
+const SearchBar = (props) => {
+
+  const handleFilter = (event) => {
+    props.filterStocks(event.target.value);
+  }
+
+  const handleSort = (event) => {
+    props.sortStocks(event.target.value);
+  }
+
   return (
     <div>
 
       <strong>Sort by:</strong>
       <label>
-        <input type="radio" value="Alphabetically" checked={null} onChange={null}/>
+        <input type="radio" value="Alphabetically" checked={props.sort === "Alphabetically" ? true : false} onChange={handleSort}/>
         Alphabetically
       </label>
       <label>
-        <input type="radio" value="Price" checked={null} onChange={null}/>
+        <input type="radio" value="Price" checked={props.sort === "Price" ? true : false} onChange={handleSort}/>
         Price
       </label>
       <br/>
 
       <label>
         <strong>Filter:</strong>
-        <select onChange={null}>
+        <select onChange={handleFilter}>
+          <option value="All">All</option>
           <option value="Tech">Tech</option>
           <option value="Sportswear">Sportswear</option>
           <option value="Finance">Finance</option>
--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -2,13 +2,22 @@ import React, { Component } from 'react';
 import Stock from '../components/Stock'
 
 class PortfolioContainer extends Component {
-
+  constructor(props) {
+    super(props);
+  }
+  renderPortFolio = () =>{
+    let stocks = [];
+    for(const stock of this.props.portFolio.values()){
+      stocks.push(<Stock sell={"sell"} sellStock={this.props.sellStock} {...stock} />)
+    }
+    return stocks;
+  }
   render() {
     return (
       <div>
         <h2>My Portfolio</h2>
           {
-            //render your portfolio stocks here
+            this.renderPortFolio()
           }
       </div>
     );
