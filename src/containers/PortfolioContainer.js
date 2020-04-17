f2969518766f5346667e054b4ff1650dbef0a9fb
ajzajac <ajzajac@icloud.com>
2020-04-17 14:35:18 -0400

    Done.

--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -3,12 +3,18 @@ import Stock from '../components/Stock'
 
 class PortfolioContainer extends Component {
 
+  renderMyStocks = () => {
+    if(this.props.myStocks){
+      return this.props.myStocks.map(stock => <Stock stock={stock} removeStock={this.props.removeStock}/>)
+    }
+  }
+
   render() {
     return (
       <div>
         <h2>My Portfolio</h2>
           {
-            //render your portfolio stocks here
+            this.renderMyStocks()
           }
       </div>
     );
--- src/containers/StockContainer.js
+++ src/containers/StockContainer.js
@@ -3,12 +3,20 @@ import Stock from '../components/Stock'
 
 class StockContainer extends Component {
 
+
+  renderStocks = () => {
+    if(this.props.stocks){
+      return this.props.stocks.map(stock => <Stock stock={stock} buyStock={this.props.buyStock} />)
+    }
+  }
+
   render() {
+    console.log(this.props.stocks)
     return (
       <div>
         <h2>Stocks</h2>
         {
-          //render the list of stocks here
+         this.renderStocks()
         }
       </div>
     );
